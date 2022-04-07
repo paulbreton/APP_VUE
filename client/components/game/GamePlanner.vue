e62ca9f1925395a6c78237eb67cdc0ccdb5bc50c
paulbreton <paul.breton@cooperl.com>
2022-04-07 14:31:29 +0200

    feat(game): page new game

--- client/components/game/GamePlanner.vue
+++ client/components/game/GamePlanner.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="new-game">
     <div class="form">
+      <div class="label-title">Titre</div>
+      <div class="input-title">
+        <el-input v-model="title" placeholder="Entrez le titre"></el-input>
+      </div>
       <div class="label-terrain">Terrain</div>
       <div class="select-terrain">
         <el-select v-model="terrain" placeholder="Sélectionnez un terrain">
@@ -10,12 +14,20 @@
       <div class="label-organisateur">Organisateur</div>
       <div class="select-orga">
         <el-select v-model="orga" placeholder="Sélectionnez un organisateur">
-          <el-option v-for="terrain in listeTerrain" :key="terrain.id" :label="terrain.nom" :value="terrain.nom" :value-key="terrain.id"></el-option>
+          <el-option v-for="orga in listeOrga" :key="orga.id" :label="orga.nom" :value="orga.id" :value-key="orga.id"></el-option>
         </el-select>
+      </div>
+      <div class="label-datapicker">Date</div>
+      <div class="datapicker">
+        <el-date-picker type="date" placeholder="Choisissez une date" v-model="date"></el-date-picker>
+      </div>
+      <div class="label-heure">Heure</div>
+      <div class="heure">
+        <el-time-picker placeholder="Pick a time" v-model="heure" style="width: 100%;"></el-time-picker>
       </div>
       <div class="label-desc">Description</div>
       <div class="input-desc">
-        <el-input type="textarea" v-model="desc"></el-input>
+        <el-input type="textarea" placeholder="Entrez votre description" v-model="desc"></el-input>
       </div>
       <div class="label-scenario">Scénario :</div>
     </div>
@@ -24,24 +36,35 @@
     </div>
     <div class="grp-btn">
       <el-button class="cancel" @click="reset">Annuler</el-button>
-      <el-button type="primary" class="submit">Confirmer</el-button>
+      <el-button type="primary" @click="submit" class="submit">Confirmer</el-button>
     </div>
   </div>  
 </template>
 
 <script>
 import { ref } from '@vue/composition-api'
-import { useRouter } from '@nuxtjs/composition-api'
+import { useContext, useRouter } from '@nuxtjs/composition-api'
 export default {
   components: {},
   setup() {
     const router = useRouter()
+    const context = useContext()
     const desc = ref('')
+    const heure = ref('')
+    const date = ref('')
+    const title = ref('')
 
     const listeTerrain = [
       { id: '1', nom: 'Mairais' },
       { id: '2', nom: 'Plouzané' },
       { id: '3', nom: 'Landerneau' },
+    ]
+
+    const listeOrga = [
+      { id: '1', nom: 'Lele' },
+      { id: '2', nom: 'Caca' },
+      { id: '3', nom: 'Gaga' },
+      { id: '6', nom: 'ADMIN' },
     ]
     const terrain = ref('')
     const orga = ref('')
@@ -50,12 +73,33 @@ export default {
       router.push('/admin')
     }
 
+    const submit = () => {
+      const newGame = {
+        title: title.value,
+        description: desc.value,
+        date: context.$dayjs(date.value).format('YYYY-MM-DD'),
+        terrain: terrain.value,
+        organisateur: orga.value,
+        draft: 1,
+        visibility: 0,
+        nb_players: 50
+      }
+      context.store.dispatch('game/storeGame', newGame)
+      router.push('/admin')
+      
+    }
+
     return {
       terrain,
       listeTerrain,
       orga,
+      listeOrga,
       desc,
-      reset
+      reset,
+      submit,
+      heure,
+      date,
+      title
     }
   },
 }
@@ -100,13 +144,15 @@ export default {
 .form {  
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr auto auto;
+  grid-template-rows: 1fr 1fr 1fr auto auto;
   grid-auto-columns: 1fr;
   gap: 1rem 2rem;
   grid-auto-flow: row;
   align-items: center;
   grid-template-areas:
+    "label-title input-title input-title input-title"
     "label-terrain select-terrain label-organisateur select-orga"
+    "label-datapicker datapicker label-heure heure"
     "label-desc input-desc input-desc input-desc"
     "label-scenario . . .";
 }
@@ -117,6 +163,10 @@ export default {
 
 .input-desc { grid-area: input-desc; }
 
+.label-input { grid-area: label-input; }
+
+.input-title { grid-area: input-title; }
+
 .label-organisateur { 
   grid-area: label-organisateur; 
   justify-self: center;
@@ -125,6 +175,17 @@ export default {
 .label-terrain { grid-area: label-terrain; }
 
 .select-orga { grid-area: select-orga; }
+
+.label-datapicker { grid-area: label-datapicker; }
+
+.datapicker { grid-area: datapicker; }
+
+.label-heure { 
+  grid-area: label-heure;
+  justify-self: center;
+}
+
+.heure { grid-area: heure; }
 
 .label-scenario { grid-area: label-scenario; }
 
